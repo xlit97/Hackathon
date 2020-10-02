cce1b4164971fdd81a099eba469b9ac81c325578
Maksim Korovin <xlit97@yandex.ru>
2020-10-02 09:41:17 +0900

    Добавил nav_graph и поэксперементировал с ним

---
+++ app/src/main/java/com/futureyakutia/hackathon/ui/FirstFragmentTest.kt
@@ -0,0 +1,9 @@
+package com.futureyakutia.hackathon.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+
+class FirstFragmentTest : NavHostFragment()
