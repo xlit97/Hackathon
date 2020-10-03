c1b2a62d192e9dfc69d59fa014b14ec3179cbd9a
fLik <akimatsuri123123Yaknet>
2020-10-03 20:36:19 +0900

    add SharingManager.kt

--- app/src/main/java/com/futureyakutia/hackathon/presentation/MainPresenter.kt
+++ app/src/main/java/com/futureyakutia/hackathon/presentation/MainPresenter.kt
@@ -1,10 +1,12 @@
 package com.futureyakutia.hackathon.presentation
 
+import dagger.hilt.android.scopes.FragmentScoped
 import moxy.InjectViewState
 import moxy.MvpPresenter
 import javax.inject.Inject
 
 @InjectViewState
+@FragmentScoped
 class MainPresenter @Inject constructor() : MvpPresenter<MainView>() {
 
     fun changeText() {
